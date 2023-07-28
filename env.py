2fc5503ca0df7dd6e8ad676ef2aa7b1ad9c9fa48
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-07-28 11:17:27 +0000

    fix folder structure issue

--- env.py
+++ env.py
@@ -6,4 +6,4 @@ os.environ['CLIENT_ORIGIN_DEV'] = 'https://3000-seaniboy200-project5rea-jc3pstdg
 os.environ['DEV'] = 'DEV'
 
 os.environ['CLOUDINARY_URL'] = 'cloudinary://869968764646451:Q1mvm_0rcD6oDfrMb8HuuPCk_HU@dgj9rjuka'
-os.environ['DATABASE_URL'] = 'postgres://hmziaipn:j8X_Xvk0vp7u0vma_rgnT4r2mlaFMMq5@trumpet.db.elephantsql.com/hmziaipn'
+os.environ['DATABASE_URL'] = 'postgres://yzjmzaph:JK8rS8g3vE_853tql28sh7EWnDjEqerR@kandula.db.elephantsql.com/yzjmzaph'
